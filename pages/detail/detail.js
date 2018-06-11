7d143e5e71f3e92485fd5c7f35a4ee8e5c4e27ed
Noah <didahuang@gmail.com>
2018-06-11 17:14:36 +0800

    ask detail

---
+++ pages/detail/detail.js
@@ -0,0 +1,12 @@
+const config = require('../../utils/config.js')
+const url = config.config.host
+const requests = require('../../utils/requests.js')
+const auth = require('../../utils/auth.js')
+Page({
+  data:{
+
+  },
+  onLoad: function(){
+    
+  },
+})
