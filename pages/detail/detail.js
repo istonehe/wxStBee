c2af4865c2e2cdde9566ed1989cb5ede06329d62
stonehe <didahuang@gmail.com>
2018-06-13 09:51:43 +0800

    moment js

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,6 +2,8 @@ const config = require('../../utils/config.js')
 const url = config.config.host
 const requests = require('../../utils/requests.js')
 const auth = require('../../utils/auth.js')
+const moment = require('../../utils/moment-with-locales.js')
+moment.locale('zh-cn')
 
 Page({
   data:{
@@ -12,20 +14,24 @@ Page({
     askimgs: [],
     askrecordfile: '',
     askvoice_duration: 0,
+    recordplaying: false,
     be_answered: false,
+    postedtime: ''
   },
   onLoad: function(){
     let that = this;
     requests.getAskDetailPromise(that, 17).then(
       function(data){
         console.log(data)
+        let postedtime = moment(data.ask.timestamp).fromNow()
         that.setData({
           pageloading: false,
           asktext: data.ask.ask_text,
           askimgs: data.ask.imgs,
-          askrecordfile: data.ask.voice_url,
+          askrecordfile: url + '/' + data.ask.voice_url,
           askvoice_duration: data.ask.voice_duration,
-          be_answered: data.ask.be_answered
+          be_answered: data.ask.be_answered,
+          postedtime: postedtime
         })
       },
       function(data){
@@ -44,6 +50,24 @@ Page({
     wx.previewImage({
       current: url + '/' + askimgs[e.currentTarget.id], // 当前显示图片的http链接
       urls: imgurls // 需要预览的图片http链接列表
+    })
+  },
+  playVoice: function () {
+    let that = this;
+    const innerAudioContext = wx.createInnerAudioContext();
+    innerAudioContext.src = that.data.askrecordfile;
+    innerAudioContext.play();
+    innerAudioContext.onPlay(() => {
+      console.log('正在播放')
+      that.setData({
+        recordplaying: true
+      });
+    })
+    innerAudioContext.onEnded(() => {
+      console.log('播放完成')
+      that.setData({
+        recordplaying: false
+      })
     })
   },
   goLogin: function () {
