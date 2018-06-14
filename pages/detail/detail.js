2778f288b47ab8d1d3d0a956f2512333a41e0a8f
stonehe <didahuang@gmail.com>
2018-06-15 00:02:12 +0800

    ask detail

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,13 +4,15 @@ const requests = require('../../utils/requests.js')
 const auth = require('../../utils/auth.js')
 const base64 = require('../../utils/base64.min.js').Base64;
 const moment = require('../../utils/moment-with-locales.js')
+const util = require('../../utils/util.js')
 moment.locale('zh-cn')
 
 Page({
-  data:{
+  data: {
     url: url,
     pageloading: true,
     auth_mask: false,
+
     asktext: '',
     askimgs: [],
     askrecordfile: '',
@@ -18,69 +20,94 @@ Page({
     recordplaying: false,
     be_answered: false,
     postedtime: '',
+
+    ask: {},
     ask_id: 0,
     answers: []
   },
-  onLoad: function(){
+  onLoad: function () {
     let that = this;
-    let ask_id = 19
+    let ask_id = 21
     requests.getAskDetailPromise(that, ask_id).then(
-      function(data){
+      function (data) {
         console.log(data)
-        console.log(data.ask.timestamp)
-        let postedtime = moment(data.ask.timestamp).add(8, 'hours').fromNow();
-        
-        let answers = []
-        
+
+        let ask = data.ask;
+        ask.postedtime = moment(data.ask.timestamp).add(8, 'hours').fromNow();
+        ask.recordplaying = false;
+
+        let answers = [];
+        let www_answers = data.ask.answers;
+        for (let i = 0, n = www_answers.length; i < n; i++) {
+          answers[i] = www_answers[i];
+          answers[i].answered_time = moment(www_answers[i].timestamp).add(8, 'hours').fromNow();
+          if (www_answers[i].teacher_imgurl){
+            answers[i].teacher_imgurl = url + '/' + www_answers[i].teacher_imgurl;
+          }
+          if (www_answers[i].student_imgurl) {
+            answers[i].student_imgurl = url + '/' + www_answers[i].student_imgurl;
+          }
+        }
 
         that.setData({
-          pageloading: false,
-          asktext: data.ask.ask_text,
-          askimgs: data.ask.imgs,
-          askrecordfile: url + '/' + data.ask.voice_url,
-          askvoice_duration: data.ask.voice_duration,
-          be_answered: data.ask.be_answered,
-          postedtime: postedtime,
-          ask_id: ask_id
+          ask: ask,
+          //asktext: data.ask.ask_text,
+          //askimgs: data.ask.imgs,
+          //askrecordfile: url + '/' + data.ask.voice_url,
+          //askvoice_duration: data.ask.voice_duration,
+          //be_answered: data.ask.be_answered,
+          //postedtime: postedtime,
+          ask_id: ask_id,
+          answers: answers,
+          pageloading: false
         })
       },
-      function(data){
+      function (data) {
         console.log(data)
       }
     )
   },
-  previewImgs: function (e) {
+  askPreviewImgs: function (e) {
     let that = this;
-    let askimgs = that.data.askimgs;
-    let imgurls = []
-    for (let i = 0, n = askimgs.length; i < n; i++) {
-      imgurls.push(url + '/' + askimgs[i])
-    }
-    console.log(url + '/' + askimgs[e.currentTarget.id])
-    wx.previewImage({
-      current: url + '/' + askimgs[e.currentTarget.id], // 当前显示图片的http链接
-      urls: imgurls // 需要预览的图片http链接列表
-    })
+    console.log(e)
+    let imgs = that.data.ask.imgs;
+    util.previewImgs(imgs, e);
+  },
+  answerPreviewImgs: function(e){
+    let that = this;
+    console.log(e)
+    let imgs = that.data.answers[e.currentTarget.dataset.parentindex].imgs;
+    util.previewImgs(imgs, e);
   },
   playVoice: function () {
     let that = this;
     const innerAudioContext = wx.createInnerAudioContext();
-    innerAudioContext.src = that.data.askrecordfile;
+    innerAudioContext.src = url + '/' + that.data.ask.voice_url;
+    console.log(url + '/' + that.data.ask.voice_url)
     innerAudioContext.play();
     innerAudioContext.onPlay(() => {
       console.log('正在播放')
+      let ask = that.data.ask;
+      ask.recordplaying = true;
       that.setData({
-        recordplaying: true
+        ask: ask
       });
-    })
+    });
     innerAudioContext.onEnded(() => {
       console.log('播放完成')
+      let ask = that.data.ask;
+      ask.recordplaying = false;
       that.setData({
-        recordplaying: false
-      })
+        ask: ask
+      });
+    });
+    innerAudioContext.onError((res) =>{
+      console.log('播放错误')
+      console.log(res.errMsg)
+      console.log(res.errCode)
     })
   },
-  deleteAsk: function(){
+  deleteAsk: function () {
     let that = this;
     let ask_id = that.data.ask_id;
     let user_info = wx.getStorageSync('user_info') || [];
@@ -92,7 +119,7 @@ Page({
       header: {
         'Authorization': 'Basic ' + base64.encode(beetoken + ':x')
       },
-      success: function(res){
+      success: function (res) {
         let data = res.data;
         console.log(data)
         if (data.code == 4) {
@@ -117,12 +144,12 @@ Page({
       function (value) {
         that.onLoad()
       }, function (error) {
-      wx.showToast({
-        title: error,
-        icon: 'none',
-        duration: 2000
-      })
-    });
+        wx.showToast({
+          title: error,
+          icon: 'none',
+          duration: 2000
+        })
+      });
     that.setData({
       auth_mask: false
     })
--- utils/util.js
+++ utils/util.js
@@ -1,5 +1,9 @@
+// util.js
 
-
+const config = require('config.js');
+const url = config.config.host;
+const school_id = config.config.school_id;
+const base64 = require('base64.min.js').Base64;
 
 const formatTime = date => {
   const year = date.getFullYear()
@@ -17,7 +21,20 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+function previewImgs(imgs, e) {
+  let imgurls = []
+  for (let i = 0, n = imgs.length; i < n; i++) {
+    imgurls.push(url + '/' + imgs[i])
+  }
+  console.log(url + '/' + imgs[e.currentTarget.id])
+  wx.previewImage({
+    current: url + '/' + imgs[e.currentTarget.id], // 当前显示图片的http链接
+    urls: imgurls // 需要预览的图片http链接列表
+  })
+}
+
 module.exports = {
+  previewImgs: previewImgs,
   formatTime: formatTime
 }
 
